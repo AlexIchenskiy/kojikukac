be03107f54706b3d8dbbc01eff994774a6b56c9e
Antonia707 <126957253+Antonia707@users.noreply.github.com>
2023-11-04 17:35:47 +0100

    fix: fix loginController

--- backend/controllers/loginController.go
+++ backend/controllers/loginController.go
@@ -24,46 +24,6 @@ type LoginResponse struct {
 	RefreshToken string `json:"refreshtoken"`
 }
 
-// Signup is a function that handles user signup
-// It takes in a gin context as an argument and binds the user data from the request body to a user struct
-// It then hashes the user's password and creates a user record in the database
-// If successful, it returns a 200 status code with a success message
-// If unsuccessful, it returns a 400 or 500 status code with an error message
-
-func Register(c *gin.Context) {
-	var user models.User
-	err := c.ShouldBindJSON(&user)
-	if err != nil {
-		log.Println(err)
-		c.JSON(400, gin.H{
-			"Error": "Invalid Inputs ",
-		})
-		c.Abort()
-		return
-	}
-	err = user.HashPassword(user.Password)
-	if err != nil {
-		log.Println(err.Error())
-		c.JSON(500, gin.H{
-			"Error": "Error Hashing Password",
-		})
-		c.Abort()
-		return
-	}
-	err = user.CreateUserRecord()
-	if err != nil {
-		log.Println(err)
-		c.JSON(500, gin.H{
-			"Error": "Error Creating User",
-		})
-		c.Abort()
-		return
-	}
-	c.JSON(200, gin.H{
-		"Message": "Sucessfully Register",
-	})
-}
-
 // Login is a function that handles user login
 // It takes in a gin context as an argument and binds the user data from the request body to a LoginPayload struct
 // It then checks if the user exists in the database and if the password is correct
