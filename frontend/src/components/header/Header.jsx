670231b71e4a4df3e5ab4793ef20fcc9616d4064
AlexIchenskiy <aichenskiy@gmail.com>
2023-11-04 15:07:37 +0100

    chore: replace static logo with a link

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { FaMapMarkedAlt } from 'react-icons/fa';
 
 import './Header.scss';
@@ -5,10 +6,10 @@ import './Header.scss';
 const Header = () => {
   return (
     <header>
-      <div className='logo-container'>
+      <Link to='/home' className='logo-container'>
         <FaMapMarkedAlt className='logo-container-logo' />
         <span className='logo-container-title'>BurekParking</span>
-      </div>
+      </Link>
       <div className='user-container'>A</div>
     </header>
   )
