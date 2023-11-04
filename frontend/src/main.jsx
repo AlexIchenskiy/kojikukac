b389c30eac03d4b70ac8be27b2b52efc84545f80
Anton Ladan <al54125@fer.hr>
2023-11-04 20:17:48 +0100

    feat: add handlers for login and register

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,48 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import './index.scss'
-import Home from './pages/Home/Home';
-import Auth from './pages/Auth/Auth';
-import Profile from './pages/Profile/Profile';
-import { ProtectedRoute } from './components/Routes/ProtectedRoute';
-import AuthProvider from './provider/authProvider';
-import { UnauthenticatedRoute } from './components/Routes/UnathenticatedRoute';
+import "./index.scss";
+import Home from "./pages/Home/Home";
+import Auth from "./pages/Auth/Auth";
+import Profile from "./pages/Profile/Profile";
+import { ProtectedRoute } from "./components/Routes/ProtectedRoute";
+import AuthProvider from "./provider/AuthProvider";
+import { UnauthenticatedRoute } from "./components/Routes/UnathenticatedRoute";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <BrowserRouter>
       <Routes>
-        <Route index element={<Navigate to='/auth' />} />
-        <Route exact path='/auth' element={<><UnauthenticatedRoute /><Auth /></>} />
-        <Route exact path='/home' element={<><ProtectedRoute /> <Home /></>} />
-        <Route exact path='/profile/:id' element={<><ProtectedRoute /><Profile /></>} />
+        <Route index element={<Navigate to="/auth" />} />
+        <Route
+          exact
+          path="/auth"
+          element={
+            <>
+              <UnauthenticatedRoute />
+              <Auth />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/home"
+          element={
+            <>
+              <ProtectedRoute /> <Home />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/profile/:id"
+          element={
+            <>
+              <ProtectedRoute />
+              <Profile />
+            </>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </AuthProvider>
-)
+);
--- frontend/src/provider/AuthProvider.jsx
+++ frontend/src/provider/AuthProvider.jsx
@@ -4,15 +4,19 @@ import AuthContext from "../context/authContext";
 
 // eslint-disable-next-line react/prop-types
 const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [token, setToken_] = useState(localStorage.getItem("token"));
+
+  const setToken = (newToken) => {
+    setToken_(newToken);
+  };
 
   useEffect(() => {
     if (token) {
       axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-      localStorage.setItem('token', token);
+      localStorage.setItem("token", token);
     } else {
       delete axios.defaults.headers.common["Authorization"];
-      localStorage.removeItem('token')
+      localStorage.removeItem("token");
     }
   }, [token]);
 
