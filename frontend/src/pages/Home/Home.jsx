de86d14737ca5af856690d478153ac90f133e270
AlexIchenskiy <aichenskiy@gmail.com>
2023-11-05 07:50:22 +0100

    fix: merge conflict

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,11 +4,7 @@ import Maps from "../../components/maps/Maps";
 import "./Home.scss";
 
 const Home = () => {
-<<<<<<< HEAD
-    /*axios
-=======
   /*axios
->>>>>>> 8624cf37e856c0dfbd702d31f6174f2b4a8fc69f
     .get("http://localhost:8080/api/user")
     .then((res) => {
       console.log(res);
